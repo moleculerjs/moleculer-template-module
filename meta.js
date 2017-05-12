d32a0807cd66f4b789f8b15ad211856da2d30314
Icebob <mereg.norbert@gmail.com>
2017-05-12 13:30:32 +0200

    add files

---
+++ meta.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const exec = require("child_process").execSync;
+
+module.exports = function(values) {
+	return {
+		questions: [
+			{
+				type: "input",
+				name: "username",
+				message: "Github username:",
+				default() {
+					return exec('git config --get user.name');
+				}
+			},		
+			{
+				type: "input",
+				name: "fullName",
+				message: "Your name:",
+				default: "Ice Services"
+			},		
+			{
+				type: "input",
+				name: "projectDescription",
+				message: "Project description:"
+			},
+			{
+				type: "input",
+				name: "serviceName",
+				message: "Service name:",
+				default() {
+					return values.projectName.replace("moleculer-", "");
+				}
+			}
+		],
+
+		completeMessage: `
+To get started:
+
+	cd {{projectName}}
+	npm run dev
+
+		`
+	};
+};
